59fc0d92f66b4845a7f0ba982fd114ff1c81f4d4
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-01-13 23:52:29 -0600

    Commit de ejercicio while

--- tema4_while.py
+++ tema4_while.py
@@ -4,7 +4,7 @@
 print("Dame un número")
 numU=input('Número:')
 x=0
-print("Su tabla de multiplicar es:")
+print("Su tabla de multiplicar es la siguiente:")
 while x<10:
     x=x+1
     result=int(numU)*int(x)
